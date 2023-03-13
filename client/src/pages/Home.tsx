31714465875708b7a9859a86c36e735515bfe864
Aryan Osqueezadeh <A02289878@aggies.usu.edu>
2023-03-13 12:38:06 -0600

    Need redirection on home page if user is already signed in

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom"
 
+// TODO: Home Page
+// If I am already logged in, then I should be redirected (replace state) to the dashboard page when I reach this page. 
+
 export const Home = () => {
   return  (
     <div className="home">
@@ -7,7 +10,7 @@ export const Home = () => {
       <h3 className="description">
         A place for herpetoculturists to observe and plan the care, cultivation, and breeding of their reptiles
       </h3>
-      
+
       <div className="login-buttons">
         <span><Link to={`/login`} className="login-links">Login </Link></span>
         <span><Link to={`/signup`} className="login-links"> Signup</Link></span>
