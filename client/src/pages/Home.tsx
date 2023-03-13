a5de7262d2d9f9a2914c7e39f8d03eef8a5f8e21
Aryan Osqueezadeh <A02289878@aggies.usu.edu>
2023-03-13 12:28:08 -0600

    Created Home Page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,32 +1,17 @@
 import { Link } from "react-router-dom"
 
-// TODO: Home Page
-// If I am already logged in, then I should be redirected (replace state) to the dashboard page when I reach this page. 
-// Otherwise I should be able to do the following:
-// I should see the name of your application
-// I should see a description of what the app does.
-// I should be able to navigate to the Login page
-// I should be able to navigate to the Signup page
-
-const users = [{
-  id: 1,
-  name: "Joseph"
-},{
-  id: 2,
-  name: "Catelyn"
-},{
-  id: 3,
-  name: "Sophie"
-}];
-
 export const Home = () => {
   return  (
-    <div>
-      {users.map(user => (
-        <div key={user.id} >
-          <Link to={`/users/${user.id}`}>{user.name}</Link>
-        </div>
-      ))}
+    <div className="home">
+      <h1>Reptile Tracker</h1>
+      <h3 className="description">
+        A place for herpetoculturists to observe and plan the care, cultivation, and breeding of their reptiles
+      </h3>
+      
+      <div className="login-buttons">
+        <span><Link to={`/login`} className="login-links">Login </Link></span>
+        <span><Link to={`/signup`} className="login-links"> Signup</Link></span>
+      </div>
     </div>
   )
 }
