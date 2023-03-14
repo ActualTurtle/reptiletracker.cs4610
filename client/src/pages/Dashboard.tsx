98b0cecfbca806e064e66de92657a6c83c714aaf
Matthew Hill <potatoman.mh@gmail.com>
2023-03-14 08:20:23 +0000

    Creation of TODO list, little bit more fleshed out organization of dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -7,67 +7,117 @@
 // I should be able to delete a reptile.
 // I should be able to log out of my account
 
+// TODO: Client-side logout page
+// TODO: Server-side logout page
+// TODO: Server-side cookies for jwt, will remove need for jwt_body in request bodies.
+// TODO: design appearance better
+// TODO: creating a reptile
+// TODO: deleting a reptile
+// TODO: selecting a reptile
+// TODO: switch between reptile and schedule view
 
 import { Reptile, Schedule } from "@prisma/client";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { useApi } from "../hooks/useApi";
 export const Dashboard = () => {
-    const navigate = useNavigate();
-    const api = useApi();
+  const navigate = useNavigate();
+  const api = useApi();
 
-    const [schedules, setSchedules] = useState<Schedule[]>([]);
-    const [reptiles, setReptiles] = useState<Reptile[]>([]);
+  const [showReptiles, setShowReptiles] = useState(false);
+  const [showSchedule, setShowSchedule] = useState(true);
+  useEffect(() => {
+    console.log(api);
+  }, []);
 
-    const [loadingSchedules, setLoadingSchedules] = useState(true);
-    const [loadingReptiles, setLoadingReptiles] = useState(true);
-
-    const [showReptiles, setShowReptiles] = useState(false);
-    const [showSchedule, setShowSchedule] = useState(true);
-    useEffect(() => {
-        console.log(api);
-
-        //get the current day
-        const date = new Date();
-        let day = date.getDay();
-        //get user's schedules
-        fetch("http://localhost:8000/users/schedule")
-            .then(response => response.json())
-            .then(res => {
-                setSchedules(res.schedules as Schedule[]);
-                setLoadingSchedules(false);
-            });
-        //get user's reptiles
-        fetch("http://localhost:8000/reptile/")
-            .then(response => response.json())
-            .then(res => {
-                setReptiles(res.reptiles as Reptile[]);
-                setLoadingReptiles(false);
-            });
-    }, []);
-
-    return (
-        <div className="page-links">
-            <span><Link to={`/logout`} className="login-links">Logout</Link></span>
-            {loadingReptiles || loadingSchedules && <p>Loading...</p>}
-            {!loadingReptiles && !loadingSchedules && showReptiles &&
-                <ReptilesDOM></ReptilesDOM>
-            }
-            {!loadingReptiles && !loadingSchedules && showSchedule &&
-                <ScheduleDOM></ScheduleDOM>
-            }
-        </div>
-    )
+  return (
+    <div>
+      <div className="page-links">
+        <span><Link to={`/logout`} className="login-links">Logout</Link></span>
+      </div>
+      <div>
+        {showReptiles &&
+          <ReptilesDOM></ReptilesDOM>
+        }
+        {showSchedule &&
+          <ScheduleDOM></ScheduleDOM>
+        }
+      </div>
+    </div>
+  )
 }
 
 const ReptilesDOM = () => {
-    return (
-        <div></div>
-    )
+  const [reptiles, setReptiles] = useState<Reptile[]>([]);
+  const [loadingReptiles, setLoadingReptiles] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/reptile/")
+      .then(response => response.json())
+      .then(res => {
+        setReptiles(res.reptiles as Reptile[]);
+        setLoadingReptiles(false);
+      });
+  }, [])
+  return (
+    <div>
+      {loadingReptiles && <p>Loading reptiles...</p>}
+      {!loadingReptiles &&
+        reptiles.map(reptile => {
+          return (
+            <div className="reptile">
+              <p>Species: {reptile.species}</p>
+              <p>Name: {reptile.name}</p>
+              <p>Sex: {reptile.sex}</p>
+              <p>Last Updated: {reptile.updatedAt.toLocaleDateString()}</p>
+              <p>Record Created At: {reptile.createdAt.toLocaleDateString()}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 const ScheduleDOM = () => {
-    return (
-        <div></div>
-    )
+  const [schedules, setSchedules] = useState<Schedule[]>([]);
+  const [loadingSchedules, setLoadingSchedules] = useState(true);
+  const [day, setDay] = useState("idk man");
+  useEffect(() => {
+    const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    setDay(days[new Date().getDay()]);
+    //get user's schedules
+    fetch("http://localhost:8000/users/schedule")
+      .then(response => response.json())
+      .then(res => {
+        setSchedules(res.schedules as Schedule[]);
+        setLoadingSchedules(false);
+      });
+  }, []);
+
+  type ScheduleKey = keyof Schedule;
+
+  return (
+    <div>
+      <p>{day}</p>
+      {loadingSchedules && <p>Loading schedules...</p>}
+      <div className="schedules">
+        {
+          schedules.map(schedule => {
+            let dayKey: ScheduleKey = day as ScheduleKey;
+            if (schedule[dayKey] === true) {
+              return (
+                <div className="schedule">
+                  <p>Reptile: {schedule.reptileId}</p>
+                  <p>Type: {schedule.type}</p>
+                  <p>Description: {schedule.description}</p>
+                  <p>Last updated: {schedule.updatedAt.toLocaleDateString()}</p>
+                </div>
+              )
+            }
+          })
+        }
+      </div>
+    </div>
+  )
 }
